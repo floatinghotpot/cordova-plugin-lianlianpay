1fdbc36f6b8d6b3a900d1dcbfed041994f3920b3
floatinghotpot <rjfun.mobile@gmail.com>
2015-05-29 18:19:10 +0800

    bugfix

--- src/ios/LLPayPlugin.h
+++ src/ios/LLPayPlugin.h
@@ -1,28 +1,16 @@
 
 #import "CDVPluginExt.h"
-#import "LLPaySdk.h"
 
-#define OPT_IS_TESTING      @"isTesting"
-#define OPT_LOG_VERBOSE     @"logVerbose"
-
-@interface LLPayPlugin : CDVPluginExt<LLPaySdkDelegate>
+@interface LLPayPlugin : CDVPluginExt
 
 - (void) setOptions:(CDVInvokedUrlCommand *)command;
 
 - (void) startPay:(CDVInvokedUrlCommand *)command;
-
-@property (assign) BOOL isTesting;
-@property (assign) BOOL logVerbose;
-
-@property (nonatomic, retain) LLPaySdk * sdk;
 
 #pragma mark virtual methods
 
 - (void)pluginInitialize;
 
 - (NSString*) __getProductShortName;
-
-- (void) parseOptions:(NSDictionary*) options;
-- (NSString*) md5:(NSString*) s;
 
 @end
