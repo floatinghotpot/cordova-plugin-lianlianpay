31a3c204f3817b6657c9db72f6549734d5ce923a
Liming Xie <liming.xie@gmail.com>
2017-02-20 16:51:48 +0800

    open source code

--- src/ios/LLPayPlugin.h
+++ src/ios/LLPayPlugin.h
@@ -1,16 +1,28 @@
 
 #import "CDVPluginExt.h"
+#import "LLPaySdk.h"
 
-@interface LLPayPlugin : CDVPluginExt
+#define OPT_IS_TESTING      @"isTesting"
+#define OPT_LOG_VERBOSE     @"logVerbose"
+
+@interface LLPayPlugin : CDVPluginExt<LLPaySdkDelegate>
 
 - (void) setOptions:(CDVInvokedUrlCommand *)command;
 
 - (void) startPay:(CDVInvokedUrlCommand *)command;
+
+@property (assign) BOOL isTesting;
+@property (assign) BOOL logVerbose;
+
+@property (nonatomic, retain) LLPaySdk * sdk;
 
 #pragma mark virtual methods
 
 - (void)pluginInitialize;
 
 - (NSString*) __getProductShortName;
+
+- (void) parseOptions:(NSDictionary*) options;
+- (NSString*) md5:(NSString*) s;
 
 @end
